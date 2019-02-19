98e519ea7ef0ed205307359385625e7b5495b6f3
Phan Cuong <kinh8do@gmail.com>
2019-02-19 18:25:43 +0700

    add user successfully

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, session, redirect, url_for, escape
-import erppeek
+import erppeek, random, string
 from unidecode import unidecode
 
 # Config Odoo server 
@@ -7,11 +7,12 @@ DATABASE='tms_2112'
 SERVER='https://cuongpv:Darkness1996@tms80-integration.trobz.com'
 USERNAME='admin'
 PASSWORD='songonight'
-domain='trobz.com' #use for genete email address
+domain='trobz.com' #use for generate email address
 
 # Config for Webserver
 app = Flask(__name__)
 app.secret_key = 'b3zLyNfTgwMERvUg' #Random string for generate session
+client=erppeek.Client(SERVER, DATABASE, USERNAME,PASSWORD)
 
 # Define for all url of application
 @app.route('/')
@@ -32,18 +33,12 @@ def login():
 # Dashboards features show
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    client=erppeek.Client(SERVER, DATABASE, USERNAME,PASSWORD)
+    if not is_user_session_exist():
+        return redirect(url_for('login'))
     data = { 
         'total_users': client.model('res.users').count(),
     }
-    
-    if request.method == 'POST' and \
-       request.form['generate_user_information']:
-       return render_template('generate_user_information.html')
-       
     return render_template('dashboard.html', data=data)
-    
-
 
 # List functions library
 @app.route('/generate_user_information', methods=['POST'])
@@ -57,8 +52,35 @@ def generate_user_information():
 
 @app.route('/tms_add_user', methods=['POST'])
 def tms_add_user():
-    data = ''
-    return data
+    # show fields
+    print(client.model('res.users').keys())
+    data = {}
+    # get partner_id if exist or create an account
+    if len(client.model('res.partner').browse(['email =' + request.form['email']]).id) == 1:
+        data['create_odoo_partner'] = "Partner has already exist, ignore this step."
+        data['partner_id'] = client.model('res.partner').browse(['email =' + request.form['email']]).id
+    else:
+        data['create_odoo_partner'] = "Look like email partner doesn't exist, created!"
+        # Let's create partner
+        data['partner_id'] = client.model('res.partner').create({
+            'email': request.form['email'],
+            'name': request.form['fullname'],
+            'parent_id': 1
+        }).id
+
+    # Create user for login TMS
+    print(data['partner_id'][0])
+    client.model('res.users').create({
+        'employer_id': 1, # equal 1 - mean trobz partner (company)
+        'partner_id': data['partner_id'][0],
+        'email': request.form['email'],
+        'login': request.form['username'],
+        'new_password': request.form['password'],
+        'daily_hour': 8,
+        'must_input_working_hour': True,
+        'group_profile_id': 53, #53: fc | dev: 55
+    })
+    return "User has created! "
 
 @app.route('/logout')
 def logout():
@@ -68,27 +90,36 @@ def logout():
 # List functions library
 ## Cover fullname to username
 def cover_fullname_to_username(fullname):
-    materials = split(fullname, ' ')
-    firstname = materials[count(materials) -1]
-    # last name
-    raw_lastname = materials.pop(count(materials-1))
-    lastname = ' '.join(raw_lastname)
+    tmp = unidecode(fullname).split(' ')
     # username 
-    username = ''.join(unidecode(firstname + lastname)).lower()
+    username = tmp[-1].lower() + ''.join([i[0] for i in tmp[:-1]]).lower()
+    # email
     email = username + "@" + domain
-    data = { 'firstname': firstname,
-        'lastname': lastname,
-        'username': username,
-        'email': email }
+    # fullname
+    raw_fullname = fullname.split()
+    #example: Phan Văn Cường => Cường (Phan Văn)
+    fullname_rule = raw_fullname[-1] + ' ' + '(' + ' '.join(raw_fullname[:-1]) + ')'
+    data = { 'username': username,
+        'email': email,
+        'fullname': fullname_rule,
+        'password': randomString(12),
+        'http_auth': username,
+        'http_pass': randomString(12),}
     return data
 
 ## Function login by slack account
 def slack_login_verify(slack_email, slack_password):
     if True:
         # Set session and dashboard redirect
+        session['slack_email'] = slack_email
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('login'))
+
+def randomString(stringLength):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
 
 def is_user_session_exist():
     if 'slack_email' in session:
